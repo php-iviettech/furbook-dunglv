a84dcb7b0e4fe10a4ed922fca5d7d49c57ac1fe3
Dun <ledungb11@gmail.com>
2017-10-23 07:29:34 +0700

    abc abc

--- config/database.php
+++ config/database.php
@@ -57,9 +57,9 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'furbook'),
+            'username' => env('DB_USERNAME', ''),
             'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
